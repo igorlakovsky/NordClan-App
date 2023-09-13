9b3bbe98ec09d00bea4bf465a3ad9e71d3ccf4b0
Igorlakovsky <igorlakovsky@yandex.ru>
2023-09-13 22:23:32 +0400

    Using docker-compose to containerization backend app (express js) and frontend app (vite-react).

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+// app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1>React Test</h1>
+    </div>
+  )
+}
